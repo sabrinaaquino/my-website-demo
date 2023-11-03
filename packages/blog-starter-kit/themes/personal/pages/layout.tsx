afc27d7538963ceec359392f8ec9803edde7070a
sabrinaaquino <sabrina.aquino@hotmail.com>
2023-11-03 05:00:14 -0300

    removing vercel analytics

--- packages/blog-starter-kit/themes/personal/pages/layout.tsx
+++ packages/blog-starter-kit/themes/personal/pages/layout.tsx
@@ -2,7 +2,6 @@ import './global.css';
 import clsx from 'clsx';
 import type { Metadata } from 'next';
 import { GeistSans, GeistMono } from 'geist/font';
-import { Analytics } from '@vercel/analytics/react';
 
 
 export const metadata: Metadata = {
@@ -39,7 +38,6 @@ export default function RootLayout({
         <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
           <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
             {children}
-            <Analytics />
           </main>
         </body>
       </html>
