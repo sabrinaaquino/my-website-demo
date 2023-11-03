71b6c5742ecee697af50bc6628dc1e6ced93fe6b
sabrinaaquino <sabrina.aquino@hotmail.com>
2023-11-03 05:39:24 -0300

    giving up on layout

--- packages/blog-starter-kit/themes/personal/pages/index.tsx
+++ packages/blog-starter-kit/themes/personal/pages/index.tsx
@@ -13,8 +13,8 @@ const Home: React.FC = () => {
   return (
     <div>
       <Header />
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Hi! I'm Sabrina</h1>
-      <p className="prose prose-neutral dark:prose-invert">I'm a human who loves math, science, solving problems, and writing code.</p>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Hi! I&rsquo;m Sabrina</h1>
+      <p className="prose prose-neutral dark:prose-invert">I&rsquo;m a human who loves math, science, solving problems, and writing code.</p>
     </div>
   );
 }
