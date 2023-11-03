765db0e1b88db164293f1f0a77d7566ee1f62515
sabrinaaquino <sabrina.aquino@hotmail.com>
2023-11-03 12:45:43 -0300

    centralizing

--- packages/blog-starter-kit/themes/personal/pages/index.tsx
+++ packages/blog-starter-kit/themes/personal/pages/index.tsx
@@ -11,10 +11,12 @@ const Home: React.FC = () => {
   }, [router]);
 
   return (
-    <div>
+    <div className="flex flex-col items-center ">
       <Header />
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Hi! I&rsquo;m Sabrina</h1>
-      <p className="prose prose-neutral dark:prose-invert">I&rsquo;m a human who loves math, science, solving problems, and writing code.</p>
+      <div className= "px-4 mt-8">
+        <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Hi! I&rsquo;m Sabrina</h1>
+        <p className="prose prose-neutral dark:prose-invert">I&rsquo;m a human who loves math, science, solving problems, and writing code.</p>
+      </div>
     </div>
   );
 }
