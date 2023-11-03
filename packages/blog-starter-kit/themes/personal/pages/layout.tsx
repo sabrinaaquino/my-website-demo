4a310fbf0f1c521e8df5a78f23f04b1b9a12d9f6
sabrinaaquino <sabrina.aquino@hotmail.com>
2023-11-03 05:06:11 -0300

    attempting to export metadata

--- packages/blog-starter-kit/themes/personal/pages/layout.tsx
+++ packages/blog-starter-kit/themes/personal/pages/layout.tsx
@@ -21,25 +21,3 @@ export const metadata: Metadata = {
     },
   };
 
-export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <html
-        lang="en"
-        className={clsx(
-          'text-black bg-white dark:text-white dark:bg-[#111010]',
-          GeistSans.variable,
-          GeistMono.variable
-        )}
-      >
-        <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-            {children}
-          </main>
-        </body>
-      </html>
-    );
-  }
