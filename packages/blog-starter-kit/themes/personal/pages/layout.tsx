768b568c83e3bc48b210f73dc8df3c8d47f26520
sabrinaaquino <sabrina.aquino@hotmail.com>
2023-11-03 04:43:11 -0300

    removing the enterprise theme from workspace

---
+++ packages/blog-starter-kit/themes/personal/pages/layout.tsx
@@ -0,0 +1,47 @@
+import './global.css';
+import clsx from 'clsx';
+import type { Metadata } from 'next';
+import { GeistSans, GeistMono } from 'geist/font';
+import { Analytics } from '@vercel/analytics/react';
+
+
+export const metadata: Metadata = {
+    metadataBase: new URL('https://sabrinaaquino.dev'),
+    title: {
+      default: 'Sabrina Aquino',
+      template: '%s | Sabrina Aquino',
+    },
+    description: 'My personal website',
+    openGraph: {
+      title: 'Sabrina Aquino',
+      description: 'My personal website',
+      url: 'https://sabrinaaquino.dev',
+      siteName: 'Sabrina Aquino',
+      locale: 'en_US',
+      type: 'website',
+    },
+  };
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <html
+        lang="en"
+        className={clsx(
+          'text-black bg-white dark:text-white dark:bg-[#111010]',
+          GeistSans.variable,
+          GeistMono.variable
+        )}
+      >
+        <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+            {children}
+            <Analytics />
+          </main>
+        </body>
+      </html>
+    );
+  }
